4f6a594f88fbcc971f0ab6c8674cbbb2afbe3fa2
RussoB22 <russo.borneo2013@example.com>
2023-05-22 10:21:12 -0500

    Added session user ID search, and fixed Events function.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -79,22 +79,6 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-// Get Session user_id
-router.get('/session', async (req, res) => {
-  // Check if the user is logged in
-  if (req.session.user_id) {
-    try {
-      // Assuming the session user data contains the user_id property
-      const user_id = req.session.user_id;
-      res.json({ user_id });
-    } catch (error) {
-      res.status(500).json({ message: 'Error retrieving user ID from session' });
-    }
-  } else {
-    res.status(401).json({ message: 'Unauthorized' });
-  }
-});
-
 
 // POST /user/login - User login
 router.post('/login', async (req, res) => {
